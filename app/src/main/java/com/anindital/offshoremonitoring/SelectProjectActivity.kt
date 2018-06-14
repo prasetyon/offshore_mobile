8840675fac1402e82654b537ba7390041196cf73
prasetyon <prstyngrhd@gmail.com>
2018-06-14 09:13:53 +0700

    First commit

---
+++ app/src/main/java/com/anindital/offshoremonitoring/SelectProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.anindital.offshoremonitoring
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectProjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_project)
+    }
+}
